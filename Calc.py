9577793901740495a940402368436748acda27f1
simon Lab <simelav24@gmail.com>
2024-04-24 00:26:59 +0300

    guides, holes

---
+++ Calc.py
@@ -0,0 +1,39 @@
+
+from pygame import Vector2
+from math import atan2, sqrt
+
+def find_collision_centers_on_line(point: Vector2, point_on_line: Vector2, line_direction: Vector2, radius: float):
+    # find distance between line and point. direction must be normalized
+    ## calculate line equation m, b
+    if line_direction[0] == 0:
+        return None
+    line_m = line_direction[1] / line_direction[0]  
+    line_b = - line_m * point_on_line[0] + point_on_line[1]
+
+    ## calculate line of point
+    if line_m == 0:
+        return None
+    p_line_m = - 1.0 / line_m
+    p_line_b = - p_line_m * point[0] + point[1]
+
+    ## find intersection
+    x_middle = (p_line_b - line_b) / (line_m - p_line_m)
+    y_middle = line_m * x_middle + line_b
+    middle = Vector2(x_middle, y_middle)
+
+    ## calculate distance
+    distance = middle.distance_to(point)
+
+    # find the points
+    arg = 4 * radius * radius - distance * distance
+    if arg < 0:
+        return None
+    median = sqrt(arg)
+    result = middle - line_direction * median
+
+    return result
+
+
+
+
+
--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import pygame
 from pygame.math import Vector2
 from math import sqrt
 from random import shuffle
+from Calc import *
+from typing import List
 
 BALL_NONE = 0
 BALL_CUE = 1
@@ -21,12 +23,15 @@ ball_color = [
 class Ball:
     _radius = 10
     _overlapping = []
-    _reg = []
-    _fakes = []
+    _reg: List['Ball'] = []
+    _fakes: List['Ball'] = []
     _collisions = []
+    _ball_to_remove: List['Ball'] = []
+    _entered_balls: List['Ball'] = []
     def __init__(self, pos=Vector2(0,0), fake=False, type=BALL_NONE):
         self.pos = pos
         self.vel = Vector2(0,0)
+        self.acc = Vector2(0,0)
         self.stable = False
         if fake:
             Ball._fakes.append(self)
@@ -37,10 +42,14 @@ class Ball:
     def set_vel(self, vel):
         self.vel = vel
         self.stable = False
+    
+    def set_acc(self, acc):
+        self.acc = acc
 
     def step(self):
-        self.pos += self.vel
+        self.vel += self.acc
         self.vel *= 0.997
+        self.pos += self.vel
 
         if abs(self.vel[0]) < 0.01:
             self.vel[0] = 0
@@ -49,6 +58,8 @@ class Ball:
         
         if self.vel == Vector2(0,0):
             self.stable = True
+        
+        self.acc *= 0
     
     def resolve_line_collision():
         for ball in Ball._reg:
@@ -103,7 +114,7 @@ class Ball:
     def draw(self):
         pygame.draw.circle(win, ball_color[self.type], self.pos, Ball._radius)
         if self.stable:
-            pygame.draw.circle(win, (255,255,0), self.pos, Ball._radius - 1, 1)
+            pygame.draw.circle(win, (0,255,0), self.pos, Ball._radius - 1, 1)
 
     def step_balls():
         for i in range(5):
@@ -112,6 +123,11 @@ class Ball:
 
             Ball.resolve_line_collision()
             Ball.resolve_ball_collisions()
+        
+        for ball in Ball._ball_to_remove:
+            Ball._entered_balls.append(ball)
+            Ball._reg.remove(ball)
+        Ball._ball_to_remove.clear()
 
     def draw_balls():
         for ball in Ball._reg:
@@ -133,6 +149,28 @@ class Line:
         for line in Line._reg:
             line.draw()
 
+class Hole:
+    _reg = []
+    def __init__(self, pos: Vector2):
+        Hole._reg.append(self)
+        self.pos = pos
+
+    def step(self):
+        for ball in Ball._reg:
+            distance = ball.pos.distance_to(self.pos)
+            if distance > 2 * Ball._radius:
+                continue
+
+            # apply force
+            force = (self.pos - ball.pos).normalize()
+            ball.set_acc((Ball._radius / distance **2) * force)
+
+            if distance < Ball._radius:
+                Ball._ball_to_remove.append(ball)
+    
+    def draw(self):
+        pygame.draw.circle(win, (0,0,0), self.pos, Ball._radius * 1.3)
+
 if __name__ == '__main__':
 
     pygame.init()
@@ -147,7 +185,9 @@ if __name__ == '__main__':
         for j in range(-i, i, 2):
             type = types.pop()
             Ball(Vector2(10 * j + offx, 10 * i * sqrt(3) + offy), type=type)
+
     Ball(Vector2(win.get_width() / 2, 200), type=BALL_CUE)
+    # Ball(Vector2(win.get_width() / 2, 400), type=BALL_TEAM1)
 
     pool_line = 350
     pool_slit = 25
@@ -169,11 +209,18 @@ if __name__ == '__main__':
 
     Line(Vector2(left + pool_d_slit, top), Vector2(right - pool_d_slit, top))
 
-    selected_ball = None
+    Hole(Vector2(left, center))
+    Hole(Vector2(right, center))
+    Hole(Vector2(left, top))
+    Hole(Vector2(right, top))
+    Hole(Vector2(left, bottom))
+    Hole(Vector2(right, bottom))
+
+    selected_ball: Ball = None
 
     done = False
     while not done:
-    # --- Main event loop
+        # --- Main event loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -186,7 +233,6 @@ if __name__ == '__main__':
                 if selected_ball:
                     power = selected_ball.pos.distance_to(pygame.mouse.get_pos())
                     direction = (selected_ball.pos - pygame.mouse.get_pos()).normalize()
-                    print(power)
                     power = min(power, 150.0)
                     selected_ball.set_vel(direction * power * 0.05)
                 selected_ball = None
@@ -198,18 +244,47 @@ if __name__ == '__main__':
         win.fill((0, 150, 0))
 
         Ball.step_balls()
+        for hole in Hole._reg:
+            hole.step()
         
+        for hole in Hole._reg:
+            hole.draw()
         Ball.draw_balls()
         Line.draw_lines()
 
+        # draw guides
         if selected_ball:
-            vec = selected_ball.pos - pygame.mouse.get_pos()
+            mouse = pygame.mouse.get_pos()
+            vec = selected_ball.pos - mouse
             pygame.draw.line(win, (255,255,0), selected_ball.pos, selected_ball.pos + vec * 3)
+
+            guide_points = []
+            for ball in Ball._reg:
+                if ball is selected_ball:
+                    continue
+                if vec.length == 0:
+                    continue
+                point = find_collision_centers_on_line(ball.pos, selected_ball.pos, vec.normalize(), Ball._radius)
+                if not point:
+                    continue
+                mouse_to_cue = selected_ball.pos - mouse
+                cue_to_guide = point - selected_ball.pos
+                if mouse_to_cue.dot(cue_to_guide) > 0:
+                    guide_points.append((point, ball))
+
+            if len(guide_points) > 0:
+                point, ball = min(guide_points, key=lambda p: selected_ball.pos.distance_squared_to(p[0]))
+                pygame.draw.circle(win, (255,255,0), point, Ball._radius, 1)
+                bounce_vec = ball.pos - point
+                pygame.draw.line(win, (255,255,0), point, point + bounce_vec * 4)
+
+        # draw entered balls
+        for i, ball in enumerate(Ball._entered_balls):
+            pos = Vector2(right + Ball._radius * 4, Ball._radius + i * 2 * Ball._radius)
+            pygame.draw.circle(win, ball_color[ball.type], pos, Ball._radius)
 
         pygame.display.update()
         clock.tick(60)
-
-
 
     pygame.quit()
     
